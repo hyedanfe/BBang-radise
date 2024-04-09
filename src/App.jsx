a1d6c7cce1ecbda72959fb7547c061b5e7b7d5e2
hyedanfe <haedan4637@gmail.com>
2024-04-10 01:23:49 +0900

    🎨 feat: QueryClientProvider 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import router from '@/routes';
 import GlobalStyle from '@styles/GlobalStyle';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyle />
       <RouterProvider router={router} />
-    </>
+    </QueryClientProvider>
   );
 }
 
