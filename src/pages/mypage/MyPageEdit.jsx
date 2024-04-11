d65313d7c96cb32ba4b09181005dc5218ecae0ac
C5D2 <yoakec5d2@naver.com>
2024-04-12 00:26:20 +0900

    ✨ mypage edit 로그아웃 기능 구현

--- src/pages/mypage/MyPageEdit.jsx
+++ src/pages/mypage/MyPageEdit.jsx
@@ -1,5 +1,28 @@
+import { useParams, useNavigate } from 'react-router-dom';
+import { useGetUserInfo } from '@hooks/queries/user';
+import Button from '@components/ui/Button';
+import useMemberStore from '@zustand/memberStore.mjs';
+
 function MyPageEdit() {
-  return <div>MyPageEdit</div>;
+  const navigate = useNavigate();
+  const { _id } = useParams();
+  const setUserData = useMemberStore((state) => state.setUser);
+
+  const handleLogout = () => {
+    setUserData(null);
+    navigate('/');
+  };
+
+  const { data } = useGetUserInfo(_id);
+
+  const item = data?.item || [];
+
+  return (
+    <>
+      <div>{item._id}</div>
+      <Button onClick={handleLogout}>로그아웃</Button>
+    </>
+  );
 }
 
 export default MyPageEdit;
