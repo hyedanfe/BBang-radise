979429ae8f5b051519aaaadcc3182c79c53aa94d
dohehehe <doheerlob@gmail.com>
2024-04-11 18:44:28 +0900

    💄 style: colors 스타일 추가

--- src/pages/home/MainPage.jsx
+++ src/pages/home/MainPage.jsx
@@ -1,18 +1,20 @@
 import Text from '@components/ui/Text';
+import { typographyMap } from '@styles/typography';
+import styled from 'styled-components';
 
 const MainPage = () => {
   return (
     <div>
-      <Text color="yellow" display="block" fontWeight="bold" typography="t1">
+      <Text color="primary01" display="block" fontWeight="bold" typography="t1">
         MainPage
       </Text>
-      {/* <PText>얍 시험해보고 있음!!</PText> */}
+      <PText>얍 시험해보고 있음!!</PText>
     </div>
   );
 };
 
-// const PText = styled.p`
-//   ${({ typography = 't1' }) => typographyMap[typography]},
-// `;
+const PText = styled.p`
+  ${({ typography = 't1' }) => typographyMap[typography]},
+`;
 
 export default MainPage;
