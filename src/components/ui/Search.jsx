9ad17d07263e9e680bd552f2f8d63c45caf75e29
hyedanfe <haedan4637@gmail.com>
2024-04-12 15:51:33 +0900

    ✨ Search.jsx 공통컴포넌트 ui 폴더에 추가 RecipeList 페이지에 검색기능 추가

---
+++ src/components/ui/Search.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import Submit from '@components/ui/Submit';
+
+Search.propTypes = {
+  onClick: PropTypes.func,
+};
+
+function Search({ onClick }) {
+  const [keyword, setKeyword] = useState('');
+
+  const handleChange = (e) => {
+    setKeyword(e.target.value);
+  };
+
+  return (
+    <form>
+      <input type="text" value={keyword} onChange={handleChange} />
+      <Submit
+        onClick={(e) => {
+          e.preventDefault();
+          onClick(keyword);
+        }}
+      >
+        검색
+      </Submit>
+    </form>
+  );
+}
+
+export default Search;
