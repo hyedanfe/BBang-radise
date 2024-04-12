550f15b3fb9cc256b517bb6afa50c1fba074b449
hyedanfe <haedan4637@gmail.com>
2024-04-13 00:18:48 +0900

    🚧 RecipeAdd페이지 작업 중

--- src/pages/recipe/RecipeAdd.jsx
+++ src/pages/recipe/RecipeAdd.jsx
@@ -1,5 +1,15 @@
+import QuillEditor from '@components/ui/QuillEditor';
+import Text from '@components/ui/Text';
+
 function RecipeAdd() {
-  return <div>RecipeAdd</div>;
+  return (
+    <>
+      <Text color="black" display="block" typography="display_l">
+        베이킹 레시피 등록하기
+      </Text>
+      <QuillEditor />
+    </>
+  );
 }
 
 export default RecipeAdd;
