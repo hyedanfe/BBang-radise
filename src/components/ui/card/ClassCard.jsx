2e681bf4e0a6a115e7ea64703c5c7f02d0d80b4c
dohehehe <doheerlob@gmail.com>
2024-04-16 21:43:25 +0900

    ✨ feat: getFormatDate 함수와 ClassCard 상태 변화 업데이트

--- src/components/ui/card/ClassCard.jsx
+++ src/components/ui/card/ClassCard.jsx
@@ -12,6 +12,7 @@ const ClassCardContainer = styled.article`
 
   &:hover .card-hover {
     display: block;
+    flex-shrink: 0;
   }
 `;
 
@@ -25,8 +26,11 @@ const ClassCardBookmark = styled.div`
 const ClassCardNavigation = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 8px;
+  gap: 10px;
   position: relative;
+  @media all and (min-width: 768px) {
+    gap: 12px;
+  }
 `;
 
 const ClassCardImgLayout = styled.div`
@@ -35,13 +39,13 @@ const ClassCardImgLayout = styled.div`
   background-color: var(--gray-07);
   border-radius: 10px;
   position: relative;
+  overflow: hidden;
 `;
 
 const ClassCardImg = styled.img`
   width: 100%;
   height: 170px;
   object-fit: cover;
-  border-radius: 10px;
 `;
 
 const ClassCardBadge = styled.div`
@@ -50,19 +54,28 @@ const ClassCardBadge = styled.div`
   left: 13px;
 `;
 
+const ClassCardImgCover = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0px;
+  left: 0px;
+  background-color: rgb(47, 34, 25);
+  mix-blend-mode: color;
+`;
+
 const ClassCardContent = styled.div`
   margin: 0px 7px;
   display: flex;
   flex-direction: column;
-  gap: 2px;
+  gap: 3px;
+
+  @media all and (min-width: 768px) {
+    gap: 6px;
+  }
 `;
 
 const ClassCardTitle = styled.div`
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  word-break: break-all;
-  font-family: pretendard, sans-serif;
   font-weight: 700;
   font-style: normal;
   font-size: 1.6rem;
@@ -72,37 +85,28 @@ const ClassCardTitle = styled.div`
 `;
 
 const H1 = styled.h1`
-  font-family: pretendard, sans-serif;
-  font-weight: 900;
-  font-style: normal;
-  font-size: 1.9rem;
-  line-height: 1.5;
-  color: var(--black);
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
   word-break: break-all;
-
-  @media all and (min-width: 768px) {
-    font-size: 1.7rem;
-  }
 `;
 
 const ClassCardHover = styled.div`
   display: none;
-  width: 9px;
-  margin-top: 4px;
-  margin-right: 6px;
+  width: 8px;
+  margin-top: 1px;
+  margin-right: 10px;
 
   @media all and (min-width: 768px) {
     width: 9.5px;
-    margin-top: 1.4px;
+    margin-top: 1.5px;
   }
 `;
 
 const ClassCardInfo = styled.div`
   display: flex;
   flex-direction: column;
+  gap: 1px;
 `;
 
 const ClassCardActive = styled.div`
@@ -120,9 +124,45 @@ ClassCard.propTypes = {
   item: PropTypes.object.isRequired,
 };
 
+// 현재 날짜를 input 날짜값과 비교 가능 형식으로 만드는 함수
+function getFormatDate(date) {
+  var year = date.getFullYear();
+  var month = 1 + date.getMonth();
+  month = month >= 10 ? month : '0' + month;
+  var day = date.getDate();
+  day = day >= 10 ? day : '0' + day;
+  return year + '-' + month + '-' + day;
+}
+
 function ClassCard({ item }) {
   const navigate = useNavigate();
+
   const mainImage = item?.mainImages[0].name;
+
+  const date = new Date();
+  const today = getFormatDate(date);
+
+  let badgeType;
+  let textColor;
+  let quantityColor;
+
+  if (item?.classAt < today || item?.endAt < today) {
+    badgeType = 'inactive';
+    quantityColor = 'gray06';
+    textColor = 'gray06';
+  } else if (item?.startAt > today) {
+    badgeType = 'queue';
+    quantityColor = 'gray07';
+    textColor = 'black';
+  } else if (item?.buyQuantity == item?.quantity) {
+    badgeType = 'closed';
+    quantityColor = 'primary02';
+    textColor = 'gray07';
+  } else {
+    badgeType = 'active';
+    quantityColor = 'primary01';
+    textColor = 'black';
+  }
 
   return (
     <ClassCardContainer>
@@ -133,35 +173,42 @@ function ClassCard({ item }) {
       <ClassCardNavigation onClick={() => navigate(`/class/${item._id}`)}>
         <ClassCardImgLayout>
           <ClassCardImg src={`${import.meta.env.VITE_API_SERVER}/files/${import.meta.env.VITE_CLIENT_ID}/${mainImage}`} alt="클래스 대표 이미지" />
+          {(item?.classAt < today || item?.endAt < today) && <ClassCardImgCover />}
           <ClassCardBadge>
-            <Badge type="active"></Badge>
+            <Badge type={badgeType}></Badge>
           </ClassCardBadge>
         </ClassCardImgLayout>
 
         <ClassCardContent>
-          <ClassCardTitle>
-            <ClassCardHover className="card-hover">
-              <ArrowIcon width="100%" stroke="var(--primary-01)"></ArrowIcon>
-            </ClassCardHover>
+          <Text color={textColor} typography="black_m">
+            <ClassCardTitle>
+              <ClassCardHover className="card-hover">
+                <ArrowIcon width="100%" stroke="var(--primary-01)"></ArrowIcon>
+              </ClassCardHover>
 
-            <H1>{item.name}</H1>
-          </ClassCardTitle>
+              <H1>
+                <Text color={textColor} typography="black_m">
+                  {item.name}
+                </Text>
+              </H1>
+            </ClassCardTitle>
+          </Text>
 
           <ClassCardInfo>
             <ClassCardActive>
-              <Text typography="bold_m" color="gray08">
-                {item.extra.classAt}
+              <Text typography="bold_m" color={textColor}>
+                {item.classAt}
               </Text>
-              <Text typography="black_s" color="primary01">
+              <Text typography="black_s" color={quantityColor}>
                 {item.buyQuantity}/{item.quantity}명
               </Text>
             </ClassCardActive>
 
             <ClassCardStatic>
-              <Text typography="extrabold_s" color="gray08">
-                {item.extra.address}
+              <Text typography="extrabold_s" color={textColor}>
+                {item.address}
               </Text>
-              <Text typography="extrabold_s" color="gray08">
+              <Text typography="extrabold_s" color={textColor}>
                 {item.seller_id}
               </Text>
             </ClassCardStatic>
