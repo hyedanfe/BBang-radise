3712dd35376b2c840b950e4eb98f38f0e8362869
C5D2 <yoakec5d2@naver.com>
2024-04-16 14:30:12 +0900

    ✨ api 훅에 multiple files 추가

--- src/hooks/apis/useFileApis.mjs
+++ src/hooks/apis/useFileApis.mjs
@@ -1,4 +1,5 @@
 import useCustomAxios from '@hooks/useCustomAxios.mjs';
+import { error } from 'jquery';
 
 const useFileApis = () => {
   const axios = useCustomAxios();
@@ -8,15 +9,32 @@ const useFileApis = () => {
     formData.append('attach', file);
 
     return axios.post('/files', formData, {
-      method: 'post',
       headers: {
         'Content-Type': 'multipart/form-data',
       },
     });
   };
 
+  const postMultipleFiles = (files) => {
+    const filesArray = Array.from(files);
+
+    if (filesArray.length > 0) {
+      const formData = new FormData();
+
+      filesArray.forEach((file) => {
+        const blob = new Blob([file], { type: file.type });
+        formData.append(`attach`, blob, file.name);
+      });
+
+      return axios.post(`/files`, formData, {
+        headers: { 'Content-Type': 'multipart/form-data' },
+      });
+    }
+  };
+
   return {
     postSingleFile,
+    postMultipleFiles,
   };
 };
 
