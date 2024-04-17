301191b85c37a2df0817dff830e7809c4f65a2ba
C5D2 <yoakec5d2@naver.com>
2024-04-17 16:46:04 +0900

    ✨ 클래스 등록 페이지 구현 중

---
+++ src/pages/class/ClassForm.jsx
@@ -0,0 +1,318 @@
+import { useForm } from 'react-hook-form';
+import { useNavigate, useParams } from 'react-router-dom';
+import Submit from '@components/ui/button/Submit';
+import Input from '@components/ui/Input';
+import TextArea from '@components/ui/TextArea';
+import useFileApis from '@hooks/apis/useFileApis.mjs';
+import Button from '@components/ui/button/Button';
+import Section from '@components/ui/Section';
+import Text from '@components/ui/Text';
+import Select from '@components/ui/Select';
+import useCustomAxios from '@hooks/useCustomAxios.mjs';
+import * as S from '@styles/class/class.style';
+import DefaultImagePreview from '@assets/DefaultImagePreview.png';
+import { useEffect, useState } from 'react';
+import { useGetClassDetail } from '@hooks/queries/class';
+
+// TODO: 파일 미리보기 공동 컴포넌트
+function ClassForm() {
+  const navigate = useNavigate();
+  // const { _id } = useParams();
+  const axios = useCustomAxios();
+  const { postSingleFile, postMultipleFiles } = useFileApis();
+  // const { data, isLoading, error } = useGetClassDetail(_id);
+
+  const {
+    register,
+    handleSubmit,
+    setError,
+    setValue,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      mainImages: '',
+      detailImages: [],
+      name: '',
+      classAt: '',
+      startAt: '',
+      endAt: '',
+      address: '',
+      quantity: '',
+      price: '',
+      content: '',
+    },
+    mode: 'onChange',
+  });
+
+  const [mainImagesPreview, setMainImagesPreview] = useState('');
+  const [detailImagesPreview, setDetailImagesPreview] = useState([]);
+  const optionData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20].map((number) => ({
+    value: number,
+    label: number,
+  }));
+
+  // const classInfo = data?.item;
+  // useEffect(() => {
+  //   if (classInfo) {
+  //     reset({
+  //       mainImages: '',
+  //       detailImages: [],
+  //       name: '',
+  //       classAt: '',
+  //       startAt: '',
+  //       endAt: '',
+  //       address: '',
+  //       quantity: '',
+  //       price: '',
+  //       content: '',
+  //     });
+  //   }
+  // }, [classInfo, reset]);
+
+  const mainImages = watch('mainImages');
+  const detailImages = watch('detailImages');
+
+  useEffect(() => {
+    if (mainImages && mainImages.length > 0) {
+      const file = mainImages[0];
+      setMainImagesPreview(URL.createObjectURL(file));
+    }
+  }, [mainImages]);
+
+  useEffect(() => {
+    if (detailImages && detailImages.length > 0) {
+      const selectedFiles = [...detailImages].slice(0, 10);
+      const files = selectedFiles.map((file) => URL.createObjectURL(file));
+      setDetailImagesPreview(files);
+      console.log(files);
+    }
+  }, [detailImages]);
+
+  const renderImageArray = [...detailImagesPreview, ...Array(10 - detailImagesPreview.length).fill(DefaultImagePreview)];
+
+  const deleteMainImage = () => {
+    setValue('mainImages', { mainImage: {} });
+    setMainImagesPreview([]);
+  };
+
+  const deleteDetailImage = (index) => {
+    const newDetailImagesPreview = detailImagesPreview.filter((_, i) => i !== index);
+    setDetailImagesPreview(newDetailImagesPreview);
+
+    console.log(detailImages);
+    const newDetailImages = [...detailImages].filter((_, i) => i !== index);
+    setValue('detailImages', newDetailImages);
+    console.log(detailImages);
+  };
+
+  const onSubmit = async (formData) => {
+    try {
+      console.log(formData);
+
+      if (formData.mainImages.length > 0) {
+        const fileRes = await postSingleFile(formData.mainImages[0]);
+        console.log(fileRes);
+        formData.mainImages = fileRes.data.item;
+      } else {
+        delete formData.mainImages;
+      }
+
+      if (formData.detailImages.length > 0) {
+        const fileRes = await postMultipleFiles(formData.detailImages);
+        console.log(detailImages);
+        console.log(fileRes);
+        formData.detailImages = fileRes.data.item.map((item) => item);
+      } else {
+        delete formData.detailImages;
+      }
+
+      const res = await axios.post(`seller/products`, formData);
+      navigate(`/class/${res?.data.item._id}`);
+    } catch (err) {
+      console.log(err);
+      if (err.response?.data.errors) {
+        err.response?.data.errors.forEach((error) => setError(error.path, { message: error.msg }));
+      } else if (err.response?.data.message) {
+        alert(err.response?.data.message);
+      }
+    }
+  };
+
+  return (
+    <Section>
+      <S.ClassAddWrapper>
+        <Text typography="display_l"> 베이킹 클래스 모집하기</Text>
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <Input type="file" accept="image/*" id="mainImages" label="대표 이미지 (1개)" placeholder="이미지를 선택하세요" {...register('mainImages')} />
+          </div>
+          <div>
+            <S.FileInputBoxStyle>
+              {mainImagesPreview && mainImagesPreview.length > 0 ? (
+                <S.PreviewImageWrapper>
+                  <S.PreviewImageStyle src={mainImagesPreview} className="hidden" width="160" height="160" alt="main_image" />
+                  <S.DeleteButtonWrapper>
+                    <S.DeleteButtonStyle type="button" onClick={deleteMainImage}>
+                      x
+                    </S.DeleteButtonStyle>
+                  </S.DeleteButtonWrapper>
+                </S.PreviewImageWrapper>
+              ) : (
+                <S.PreviewImageWrapper>
+                  <S.PreviewImageStyle src={DefaultImagePreview} alt="기본이미지" />
+                </S.PreviewImageWrapper>
+              )}
+            </S.FileInputBoxStyle>
+          </div>
+          <div>
+            <Input type="file" multiple accept="image/*" id="detailImages" label="상세 이미지 (최대 10개)" placeholder="이미지를 선택하세요" {...register('detailImages')} />
+          </div>
+          <S.FileInputBoxStyle>
+            {detailImagesPreview && detailImagesPreview.length > 0
+              ? renderImageArray.slice(0, 10).map((url, i) => (
+                  <S.PreviewImageWrapper key={i}>
+                    <S.PreviewImageStyle src={url} className="hidden" width="160" height="160" alt={`image${i}`} />
+                    {url !== DefaultImagePreview && (
+                      <S.DeleteButtonWrapper>
+                        <S.DeleteButtonStyle type="button" onClick={() => deleteDetailImage(i)}>
+                          x
+                        </S.DeleteButtonStyle>
+                      </S.DeleteButtonWrapper>
+                    )}
+                  </S.PreviewImageWrapper>
+                ))
+              : [...Array(10)].map((_, i) => (
+                  <S.PreviewImageWrapper key={i}>
+                    <S.PreviewImageStyle src={DefaultImagePreview} alt="기본이미지" />
+                  </S.PreviewImageWrapper>
+                ))}
+          </S.FileInputBoxStyle>
+          <div>
+            <Input
+              type="text"
+              id="name"
+              label="제목 (30자 이내)"
+              height="40px"
+              placeholder="제목을 입력하세요"
+              error={errors.name && errors.name.message}
+              {...register('name', {
+                required: '제목을 입력하세요.',
+                minLength: {
+                  value: 2,
+                  message: '제목을 2자 이상 입력하세요.',
+                },
+                maxLength: {
+                  value: 30,
+                  message: '제목을 30자 이내 입력하세요.',
+                },
+              })}
+            />
+          </div>
+          <div>
+            <Input
+              type="date"
+              id="classAt"
+              label="클래스 일자"
+              placeholder="연/월/일"
+              error={errors.classAt && errors.classAt.message}
+              {...register('classAt', {
+                required: '클래스 일자를 입력하세요.',
+              })}
+            />
+            <Input
+              type="date"
+              id="startAt"
+              label="모집 시작일"
+              placeholder="연/월/일"
+              error={errors.startAt && errors.startAt.message}
+              {...register('startAt', {
+                required: '모집 시작일을 입력하세요.',
+              })}
+            />
+            <Input
+              type="date"
+              id="endAt"
+              label="모집 종료일"
+              placeholder="연/월/일"
+              error={errors.endAt && errors.endAt.message}
+              {...register('endAt', {
+                required: '모집 종료일을 입력하세요.',
+              })}
+            />
+            <Input
+              type="text"
+              id="address"
+              label="클래스 장소"
+              height="40px"
+              placeholder="클래스 장소를 입력하세요"
+              error={errors.address && errors.address.message}
+              {...register('address', {
+                required: '클래스 장소를 입력하세요.',
+                minLength: {
+                  value: 2,
+                  message: '클래스 장소를 2자 이상 입력하세요.',
+                },
+              })}
+            />
+            <Select
+              id="quantity"
+              label="클래스 참여 인원"
+              height="40px"
+              placeholder="인원을 선택해주세요"
+              optionData={optionData}
+              error={errors.quantity && errors.quantity.message}
+              {...register('quantity', {
+                required: '인원을 선택해주세요.',
+              })}
+            />
+          </div>
+          <Input
+            type="number"
+            id="price"
+            min="0"
+            label="클래스 참여 비용"
+            height="40px"
+            placeholder="참여 비용을 입력하세요"
+            error={errors.price && errors.price.message}
+            {...register('price', {
+              required: '참여 비용을 입력하세요.',
+            })}
+          />
+          <div>
+            <TextArea
+              label="클래스 세부 내용"
+              type="txt"
+              id="content"
+              placeholder="텍스트를 입력해주세요"
+              {...register('content', {
+                required: '클래스 세부 내용을 입력하세요.',
+                minLength: {
+                  value: 10,
+                  message: '클래스 세부 내용을 10자 이상 입력하세요.',
+                },
+              })}
+            />
+          </div>
+          <div>
+            <Button
+              onClick={() => {
+                navigate(-1);
+              }}
+            >
+              취소하기
+            </Button>
+          </div>
+
+          <div>
+            <Submit>개설하기</Submit>
+          </div>
+        </form>
+      </S.ClassAddWrapper>
+    </Section>
+  );
+}
+
+export default ClassForm;
