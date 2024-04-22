7c8c1ab77e37b8c3f4edb9a7c242bce903111e79
dohehehe <doheerlob@gmail.com>
2024-04-22 22:34:51 +0900

    💄 style: SignUp 컴포넌트 업데이트

--- src/styles/signup/signup.style.js
+++ src/styles/signup/signup.style.js
@@ -2,5 +2,42 @@ import styled from 'styled-components';
 
 export const SignUpWrapper = styled.div`
   width: 100%;
-  padding-top: 96px;
+  position: reltaive;
+  padding-top: 100px;
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+
+  @media all and (min-width: 768px) {
+    position: relative;
+    padding-top: 120px;
+    max-width: 500px;
+  }
+`;
+
+export const SignUpForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+
+  @media all and (min-width: 768px) {
+    width: 86%;
+  }
+`;
+
+export const SignUpInputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  margin-bottom: 40px;
+`;
+
+export const SignUpEmailWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 17px;
+`;
+
+export const SignUpValidation = styled.div`
+  margin-left: auto;
 `;
