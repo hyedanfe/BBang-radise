385c76e140e30016799a5851c878de210eb33b53
C5D2 <yoakec5d2@naver.com>
2024-06-14 17:08:12 +0900

    ✨ admin: 등업 조건 충족 회원 리스트 필터 추가

--- src/pages/admin/MemberList.jsx
+++ src/pages/admin/MemberList.jsx
@@ -6,7 +6,7 @@ import SimpleButton from '@pages/mypage/SimpleButton';
 import * as S from '@styles/admin/memberList.style';
 import { useInfiniteQuery } from '@tanstack/react-query';
 import useMemberStore from '@zustand/memberStore.mjs';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import useIntersectionObserver from '@hooks/useIntersectionObserver';
 import { Loader } from '@components/ui/Loading';
 
@@ -18,12 +18,16 @@ function MemberList() {
   const ref = useRef(null);
   const pageRef = useIntersectionObserver(ref, {});
   const isPageEnd = !!pageRef?.isIntersecting;
+  const [isUpgradeUserList, setIsUpgradeUserList] = useState(false);
 
   const fetchStores = async ({ pageParam = 1 }) => {
+    const custom = isUpgradeUserList ? JSON.stringify({ 'extra.confirm': false }) : undefined;
+
     const { data } = await axios(`admin/users`, {
       params: {
         limit: import.meta.env.VITE_USER,
         page: pageParam,
+        custom: custom,
       },
     });
     console.log(data);
@@ -36,7 +40,7 @@ function MemberList() {
     fetchNextPage,
     hasNextPage,
   } = useInfiniteQuery({
-    queryKey: ['userList'],
+    queryKey: ['userList', isUpgradeUserList],
     queryFn: fetchStores,
     suspense: true,
     getNextPageParam: (lastPage) => lastPage.nextPage,
@@ -55,6 +59,10 @@ function MemberList() {
     clearUserDataStorage();
   };
 
+  const handleUpgradeToggle = () => {
+    setIsUpgradeUserList((prevMode) => !prevMode);
+  };
+
   return (
     <Section>
       <S.AdminWrapper>
@@ -62,6 +70,7 @@ function MemberList() {
           <Text typography="display_l" display="block" color="black">
             회원 목록
           </Text>
+          <SimpleButton onClick={handleUpgradeToggle}>{isUpgradeUserList ? '전체 회원' : '등업 가능 회원'} </SimpleButton>
           <SimpleButton onClick={handleLogout}>로그아웃</SimpleButton>
         </S.AdminHeader>
 
